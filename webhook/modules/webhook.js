3cfda5506fed0a2506d664eba8939e0b38c05049
J. Enrique Peñaloza <mundoenrique@hotmail.com>
2018-04-09 00:03:16 -0400

    Mejora sintaxis

--- webhook/modules/webhook.js
+++ webhook/modules/webhook.js
@@ -4,6 +4,7 @@ const
   config = require('config'),
   moment = require('moment'),
   API = require('./connectAPIS'),
+  EVENTS = require('./handleEvents'),
   //Token de validación del webhook
   WEBHOOK_TOKEN = (process.env.WEBHOOK_TOKEN) ? process.env.WEBHOOK_TOKEN : config.get('webHookToken');
 /*
@@ -23,42 +24,46 @@ var verifyWebhook = (req, res) => {
 var webhookMessaging = (req, res) => {
   let body = req.body;
 
-  //Validar que se un petición desde la página
+  //Válida que sea una petición desde la página
   if (body.object == 'page') {
-    console.log(body.entry[0].messaging[0]);
     let pageID = body.entry[0].id;
     let timeOfEvent = new Date(body.entry[0].time);
     let senderId = body.entry[0].messaging[0].sender.id;
     let messagingEvent = body.entry[0].messaging[0];
     let webhookEvent;
     API.getFacebookDataUser(senderId, function(userData){
-      console.log(userData)
-      if (messagingEvent.optin) {
-          webhookEvent = 'optin';
-          //receivedAuthentication(messagingEvent);
-        } else if (messagingEvent.message) {
-          webhookEvent = '\"message\"';
-          //receivedMessage(messagingEvent);
-        } else if (messagingEvent.delivery) {
-          webhookEvent = '\"delivery\"';
-          //receivedDeliveryConfirmation(messagingEvent);
-        } else if (messagingEvent.postback) {
-          webhookEvent = '\"postback\"';
-          //receivedPostback(messagingEvent);
-        } else if (messagingEvent.read) {
-          webhookEvent = '\"read';
-          //receivedMessageRead(messagingEvent\");
-        } else if (messagingEvent.account_linking) {
-          webhookEvent = '\"account_linking\"';
-          //receivedAccountLink(messagingEvent);
-        } else {
-          console.log("Se recibio un evento desconocido", messagingEvent);
-        }
+
+      if (messagingEvent.message) {
+        webhookEvent = '\"message\"';
+
+      } else if (messagingEvent.postback) {
+        webhookEvent = '\"postback\"';
+
+      } else if (messagingEvent.delivery) {
+        webhookEvent = '\"delivery\"';
+
+      } else if (messagingEvent.optin) {
+        webhookEvent = '\"optin\"';
+
+      } else if (messagingEvent.read) {
+        webhookEvent = '\"read\"';
+
+      } else if (messagingEvent.account_linking) {
+        webhookEvent = '\"account_linking\"';
+
+      } else {
+        webhookEvent = '\"desconicido\"';
+
+      }
+      console.log('<<<<====%s Evento webhook %s recibido====>>>>', moment(timeOfEvent).format("YYYY-MM-DD HH:mm:ss"), webhookEvent);
+      console.log(messagingEvent);
+      console.log('---------------------------------------------------------------------');
     });
-    console.log('-------------------------------------------------------');
-    console.log('%s Evento webhook recibido %s', moment(timeOfEvent).format("YYYY-MM-DD HH:mm:ss"), webhookEvent);
-    //Es necesario responder (200) para indicarle a facebbok que se recibio la solicitud correctamente.
+    //Devolver '200 (process ok)' a todos los eventos.
     res.sendStatus(200);
+  } else {
+    //Devolver '404 (Not Found)' si el evento no es de una suscripción a la página
+    res.sendStatus(404);
   }
 }
 
