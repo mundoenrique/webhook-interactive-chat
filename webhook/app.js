fabcca36c2a0c3a11ad4bd7dfc7c4d18aad6743a
J. Enrique Peñaloza <mundoenrique@hotmail.com>
2018-04-08 20:36:45 -0400

    Verifica la firma de la solicitud

--- webhook/app.js
+++ webhook/app.js
@@ -7,11 +7,14 @@
  const
   bodyParser = require('body-parser'),
   config = require('config'),
+  crypto = require('crypto'),
   express = require('express'),
   request = require('request'),
   webhookRoutes = require('./routes/webhookRoutes'),
   //Identificaodr de la aplicación messenger
   MSN_APP_ID = (process.env.MSN_APP_ID) ? process.env.MSN_APP_ID : config.get('msnAppId'),
+  //Clave secreta de la aplicación MESSENGER
+  MSN_APP_SECRET = (process.env.MSN_APP_SECRET) ? process.env.MSN_APP_SECRET : config.get('msnAppSecret'),
   //Pagina de faebook asociada al bot
   FACEBOOK_PAGE = (process.env.FACEBOOK_PAGE) ? process.env.FACEBOOK_PAGE : config.get('faceBookPage'),
   //Url de la aplicación node
@@ -23,7 +26,7 @@ var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({verify: verifyRequestSignature}));
 app.use(express.static('assets'));
 
 //Recibir peticiones GET
@@ -32,6 +35,36 @@ app.get('/', (req, res) => {
 });
 
 app.use('/webhook', webhookRoutes);
+
+//Verifica que la solicitud proviene de facebook a través de la clave secreta de la aplicación
+function verifyRequestSignature(req, res, buf) {
+  console.log('<<<<====Verificando firma de la solicitud====>>>');
+  let signature = req.headers["x-hub-signature"];
+  let msg = 'La firma es VÁLIDA';
+  let valid = true;
+
+  if (!signature) {
+    msg = 'No existe la firma en la solictud.';
+    valid = false;
+
+  } else {
+    let elements = signature.split('=');
+    let method = elements[0];
+    let signatureHash = elements[1];
+
+    let expectedHash = crypto
+      .createHmac('sha1', MSN_APP_SECRET)
+      .update(buf)
+      .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      msg = 'La firma de la solicitud NO ES VÁLIDA.';
+      valid = false;
+    }
+  }
+  console.log(msg);
+  console.log('------------------------------------------------');
+}
 
 module.exports = app;
 
