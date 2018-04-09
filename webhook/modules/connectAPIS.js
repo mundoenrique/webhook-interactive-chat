2f497d07383d92aab9655c24a5e78302eeeb2dcb
J. Enrique Peñaloza <mundoenrique@hotmail.com>
2018-04-08 20:40:56 -0400

    Solicita datos públicos del usuario

--- webhook/modules/connectAPIS.js
+++ webhook/modules/connectAPIS.js
@@ -3,20 +3,41 @@ const
   //Dependencias
   config = require('config'),
   request = require('request'),
-  //Clave secreta de la aplicación MESSENGER
-  MSN_APP_SECRET = (process.env.MSN_APP_SECRET) ? process.env.MSN_APP_SECRET : config.get('msnAppSecret'),
   //Token generado por el portal de facebook developers
   MSN_ACCESS_TOKEN = (process.env.MSN_ACCESS_TOKEN) ? process.env.MSN_ACCESS_TOKEN : config.get('msnAccessToken'),
   //API ´de facebook
   FACEBOOK_API = (process.env.FACEBOOK_API) ? process.env.FACEBOOK_API : config.get('faceBookAPI');
 
+//Solicita datos públicos del usuario a facebook
 var getFacebookDataUser = (senderId, _setDatauser_) => {
+  console.log('<<<<====REQUEST datos públicos del usuario====>>>>');
+  console.log('senderID:', senderId);
+  console.log('--------------------------------------------------');
+  request({
+    method: 'GET',
+    url: FACEBOOK_API + senderId,
+    qs: {
+      access_token: MSN_ACCESS_TOKEN
+    },
+  }, (error, response, data) => {
+    console.log('<<<<====RESPONSE datos públicos del usuario====>>>>');
+    let fail = '';
 
-  _setDatauser_(senderId)
+    if(error !== null || response.statusCode !== 200) {
+      fail = error != null ? error : fail;
+    }
+
+    console.log('statusCode:', response.statusCode);
+    console.log(data, fail);
+    console.log('---------------------------------------------------');
+    _setDatauser_(data)
+  });
+
 }
 
 //Configura inicio del bot
 var setGreetingMenu = (header, body) => {
+  console.log('<<<<====Activar botón \"Empezar\", saludo y menú persistente====>>>>');
   request({
     method: 'POST',
     headers: header,
@@ -24,7 +45,6 @@ var setGreetingMenu = (header, body) => {
     qs: {access_token: MSN_ACCESS_TOKEN},
     json: body
   }, (error, response, body) => {
-    console.log('<<<<====Activar botón \"Empezar\", saludo y menú persistente====>>>>');
     let fail = '';
 
     if(error !== null || response.statusCode !== 200) {
@@ -33,7 +53,7 @@ var setGreetingMenu = (header, body) => {
 
     console.log('statusCode:', response.statusCode);
     console.log(body, fail);
-    console.log('-----------------------------------------------------------');
+    console.log('------------------------------------------------------------------');
 
   });
 }
