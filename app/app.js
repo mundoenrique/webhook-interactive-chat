088baa771230b86d04422d74445b3eddf2bebc83
Enrique Peñaloza <epenaloza@novopayment.com>
2018-04-11 18:00:32 -0400

    Corrige ruta del express.static

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json({verify: verifyRequestSignature}));
-app.use(express.static('assets'));
+app.use(express.static('app/assets'));
 
 //Recibir peticiones GET
 app.get('/', (req, res) => {
